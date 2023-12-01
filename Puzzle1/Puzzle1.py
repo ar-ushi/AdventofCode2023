c30bb0ee3b0fe0858fdb929d77bccaef57247b63
Arushi <aggarwalarushi5@gmail.com>
2023-12-01 19:54:45 +0530

    add second solution for python puzzle 1 part 1

--- Puzzle1/Puzzle1.py
+++ Puzzle1/Puzzle1.py
@@ -5,7 +5,16 @@ input = open('Puzzle1-Input.txt', 'r')
 inputLines = input.readlines()
 regex = r'[a-zA-Z\n]+'
 sum = 0
+#Solution 1
 for line in inputLines:
     line = re.sub(regex,'',line)
     sum += int(line[0] + line[-1])
 print(sum)
+
+
+#Solution 2
+total = 0
+for line in inputLines:
+    digits = [char for char in line if char.isnumeric()]
+    total += int(digits[0] + digits[-1])
+print(total)
